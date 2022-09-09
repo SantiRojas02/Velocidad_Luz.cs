357966485735c5c3b7abef01594797c486b7dd3f
SantiRojas <104436569+SantiRojas02@users.noreply.github.com>
2022-09-08 21:55:49 -0300

    Velocidad_luz

---
+++ Velocidad_Luz.cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Velocidad_Luz.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el tiempo en segundos: ");
+            int segundos = Convert.ToInt32(Console.ReadLine());
+
+            int kilometros, millas, dato1, dato2;
+
+            segundos = 3600;
+            millas=1852;
+            kilometros=27777778;
+
+            dato1 = segundos * kilometros;
+            dato2 = segundos * millas;
+
+            Console.WriteLine("Distancia en Kilometros: " + Convert.ToDecimal(segundos/kilometros));
+            Console.WriteLine("Distancia en Millas Nauticas: " + Convert.ToDecimal(segundos/millas));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
